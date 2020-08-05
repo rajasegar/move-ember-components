e7f75a1c70a47726b2dad6ff1503e196478dface
Rajasegar <rajasegarchandran@FWS-CHE-LT-4034.local>
2020-08-05 06:15:48 +0530

    feat: prompts for selecting components

--- bin/cli.js
+++ bin/cli.js
@@ -3,40 +3,70 @@
 "use strict";
 
 const moveComponent = require("../");
+const prompts = require("prompts");
+const walkSync = require("walk-sync");
 
-const componentName = process.argv[2];
-const destination = process.argv[3];
-const dryRun =
-  process.argv.includes("--dry-run") || process.argv.includes("-d");
-const pods = process.argv.includes("--pods");
-const options = {
-  componentName,
-  destination,
-  dryRun,
-  pods: true, // hard coding pods TODO: remove later
-};
+const _components = walkSync("app/components", {
+  directories: false,
+  globs: ["**/component.js"],
+});
+const components = _components.map((c) => {
+  return { title: c.replace("/component.js", "") };
+});
 
-// Move components
-moveComponent(options);
+const questions = [
+  {
+    type: "autocomplete",
+    name: "componentName",
+    message: "Select your component",
+    choices: components,
+  },
+  {
+    type: "text",
+    name: "destination",
+    message: "Where you want to move ?",
+  },
+];
 
-console.log("Running Codemods: ");
-console.log("------------------");
-// Run codemod
-if (!dryRun) {
-  // Run template codemods
-  require("codemod-cli").runTransform(
-    __dirname,
-    "rename-components",
-    `app/**/*.hbs   --from=${componentName} --to=${destination}`,
-    "hbs"
-  );
+(async () => {
+  const response = await prompts(questions);
 
-  // Run js codemods
+  const { componentName, destination } = response;
+  const dryRun =
+    process.argv.includes("--dry-run") || process.argv.includes("-d");
+  const pods = process.argv.includes("--pods");
+  const options = {
+    componentName,
+    destination,
+    dryRun,
+    pods: true, // hard coding pods TODO: remove later
+  };
 
-  require("codemod-cli").runTransform(
-    __dirname,
-    "rename-in-tests",
-    `tests/**/*.js  --from=${componentName} --to=${destination}`,
-    "js"
-  );
+  // Move components
+  moveComponent(options);
+  runCodemods();
+})();
+
+function runCodemods() {
+  // Run codemod
+  if (!dryRun) {
+    console.log("Running Codemods: ");
+    console.log("------------------");
+
+    // Run template codemods
+    require("codemod-cli").runTransform(
+      __dirname,
+      "rename-components",
+      `app/**/*.hbs   --from=${componentName} --to=${destination}`,
+      "hbs"
+    );
+
+    // Run js codemods
+    require("codemod-cli").runTransform(
+      __dirname,
+      "rename-in-tests",
+      `tests/integration/components/**/*.js  --from=${componentName} --to=${destination}`,
+      "js"
+    );
+  }
 }
--- transforms/rename-in-tests/index.js
+++ transforms/rename-in-tests/index.js
@@ -7,20 +7,28 @@ module.exports = function transformer(file, api) {
 
   const root = j(file.source);
 
+  const newString = `${options.to}/${options.from}`;
+
+  // Replacing in hbs`` tagged template literals
   root
     .find(j.TaggedTemplateExpression)
     .filter((path) => path.value.tag.name === "hbs")
     .forEach((path) => {
       const newQuasis = path.value.quasi.quasis.map((q) => {
-        const raw = q.value.raw.replace(
-          options.from,
-          `${options.to}/${options.from}`
-        );
+        const raw = q.value.raw.replace(options.from, newString);
         q.value.raw = raw;
         return q;
       });
 
       path.value.quasi.quasis = newQuasis;
+    });
+
+  // Replacing literal values
+  root
+    .find(j.Literal)
+    .filter((path) => typeof path.value.value === "string")
+    .forEach((path) => {
+      path.value.value = path.value.value.replace(options.from, newString);
     });
 
   return root.toSource();
